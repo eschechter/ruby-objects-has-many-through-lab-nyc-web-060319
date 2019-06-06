847d3f7c1335f7f9256c93f94729fa07883b2d2c
eschechter <schechter.evan@gmail.com>
2019-06-06 16:20:40 -0400

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,18 @@
+class Appointment
+
+    @@all = []
+
+    attr_reader :doctor, :patient
+    attr_accessor :date
+
+    def initialize(date, patient, doctor)
+        @date = date
+        @patient = patient
+        @doctor = doctor
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+
+    @@all = []
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def genres
+        self.songs.map do |song_by_artist|
+            song_by_artist.genre
+        end.uniq
+    end
+end
